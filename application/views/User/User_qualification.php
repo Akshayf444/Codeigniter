052e2361a1fba442efac6028855bce8bdbfd311b
Akshayf444 <mohite.akshay118@gmail.com>
2015-10-14 17:21:35 +0530

    remove addmore

--- application/views/User/User_qualification.php
+++ application/views/User/User_qualification.php
@@ -3,7 +3,8 @@
 <div class="row"><?php echo validation_errors(); ?></div>
 <div class="row">
     <div class="col-lg-3"></div>
-    <div class="panel panel-default col-lg-6 one">
+    <div class="panel panel-default col-lg-6 ">
+        <div class="one">
         <div class="form-group " style="text-align: center">
             <h3>Highest Qualification</h3>
         </div>
@@ -29,9 +30,11 @@
             </select>
         </div>
         
-        <label id="add" class="pull-right add" >AddMore +</label>
-        <label class="pull-right del" style="display: none">Delete -</label>
-       <div class="form-group"> 
+<!--        <label id="add" class="pull-right add" >AddMore +</label>
+        <label class="pull-right del" style="display: none">Delete -</label>-->
+       
+    </div>
+        <div class="form-group"> 
         <input type="submit" class="btn btn-success" value="Add Qualification"/>
         </div>
     </div>
@@ -41,11 +44,5 @@
 </form>
 <script><?php echo $dropdowns[2]; ?></script>
 <script >
-    $(document).ready(function(){
-    $('#add').click(function(){
- 
-    $('.one').append("<div class='second' ><hr style='border: 1px solid;'><div class='form-group' style='text-align: center'><h3>Second Highest Qualification</h3></div><div class='form-group'><label class='control-label'>Second Highest Qualification Held</label><input type='text' name='qualification' class='form-control'/></div><div class='form-group'><label class='control-label'>Specialization</label><input type='text' name='specialization' class='form-control'/></div><div class='form-group'><label class='control-label'>Year</label><input type='text' name='year' class='form-control'/></div><div class='form-group'><label class='control-label'>Institute</label><input type='text' name='year' class='form-control'/></div></div>")
-    })
     
-    })
 </script>
