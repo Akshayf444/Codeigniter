7341f3d85364b7a00e229e31e926d56cf7841770
Akshayf444 <mohite.akshay118@gmail.com>
2015-10-17 12:42:17 +0530

    Changed address model

--- application/models/address_model.php
+++ application/models/address_model.php
@@ -8,32 +8,23 @@ class address_model extends CI_Model {
 
     public function add_address($id) {
         $query = $this->db->get_where('address_master', array('auth_id' => $id));
-        $data3 = array('auth_id' => $this->input->post('auth_id'),
+
+        $field_array = array(
+            'auth_id' => $id,
             'address1' => $this->input->post('address1'),
             'pincode' => $this->input->post('pincode'),
             'state' => $this->input->post('state'),
             'city' => $this->input->post('city'),
-            'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
         );
-        $data4 = array('auth_id' => $this->input->post('auth_id'),
-            'address1' => $this->input->post('address1'),
-            'pincode' => $this->input->post('pincode'),
-            'state' => $this->input->post('state'),
-            'city' => $this->input->post('city'),
-//            add_pincode();
-            'created_at' => date('Y-m-d H:i:s'),
-            'updated_at' => date('Y-m-d H:i:s'),
-        );
+
         if (!empty($query->num_rows() > 0)) {
-            $this->db->where('auth_id', $id);
-            return $this->db->update('address_master', $data3);
+            return $this->db->update('address_master', $field_array);
         } else {
-            return $this->db->insert('address_master', $data4);
+            $field_array['created_at'] = date('Y-m-d H:i:s');
+            return $this->db->insert('address_master', $field_array);
         }
     }
-
-   
 
     public function find_id($id) {
         $query = $this->db->get_where('address_master', array('auth_id' => $id));
