2497e06b81792a4f178ee1f1d5ceaf8450b92762
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-10-19 17:50:01 +0530

    small changes

--- application/views/User/other.php
+++ application/views/User/other.php
@@ -5,7 +5,7 @@
     <div class="col-lg-12">
         <div class="form-group"><h5>language Known</h5></div>
         <div class="form-group">
-            <table class="table">
+            <table class="table lan">
                 <tr>
                     <td>language</td>
                     <td>Proficiency Level</td>
@@ -13,7 +13,7 @@
                     <td>Write</td>
                     <td>Speak</td>
                 </tr>
-                <tr>
+                <tr class="add">
                     <td><input type="text" name="language"/></td>
                     <td>
                         <select>
@@ -28,8 +28,16 @@
                     <td><input type="checkbox" name="speak"/></td>
                 </tr>
             </table>
+            <button class="btn addbtn">ADD MORE</button>
         </div>
     </div>
 
 </div>
 </form>
+<script>
+    $(document).ready(function(){
+        $('.addbtn').click(function(){
+            $('.lan').append('<tr class="add1"><td><input type="text" name="language"/></td><td><select><option value="">select</option><option value="beginner">Beginner</option><option value="expert">Expert</option><option value="proficient">proficient</option></select></td><td><input type="checkbox" name="read"/></td><td><input type="checkbox" name="write"/></td><td><input type="checkbox" name="speak"/></td></tr>');
+        })
+    })
+</script>
