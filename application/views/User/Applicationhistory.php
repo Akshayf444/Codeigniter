b4ea9c33523896f2f86b844027578e9551bc31de
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-10-26 17:47:04 +0530

    small changes

--- application/views/User/Applicationhistory.php
+++ application/views/User/Applicationhistory.php
@@ -1,23 +1,29 @@
 <h2 class="page-header">Application History</h2>
 <div class="row">
-    <div class="col-lg-6">
+    <div class="col-lg-6 panel panel-default">
         <table>
-            <tr>
-            <th>Job Details</th>
-            <th>Applied On</th>
-            </tr>
-            <?php foreach ($history as $application):?>
-            <tr>
-                <td>
-                    <div class="row">
-                    <span><?php echo $application->title;?></span>
-                    </div>
-                    <div class="row">
-                    <span><?php echo $application->name;?></span>
-                    </div>
-                </td>
-            </tr>
-            <?php endforeach;?>
+            <thead>
+                <tr>
+                    <th>Job Details</th>
+                    <th>Applied On</th>
+                </tr>
+            </thead>
+         
+            
+            <tbody>
+                <?php foreach ($history as $application): ?>
+                    <tr>
+                        <td>
+                            <div class="row">
+                                <span><?php echo $application->title; ?></span>
+                            </div>
+                            <div class="row">
+                                <span><?php echo $application->name; ?></span>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
         </table>
     </div>
 </div>
