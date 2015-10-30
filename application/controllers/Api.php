d62a9af75e0bf23d8df034619ec6f064fe26df19
raiyan2723 <raiyanshaikh1993@gmail.com>
2015-10-30 15:12:10 +0530

    small changes

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -16,14 +16,11 @@ class Api extends CI_Controller {
         $check = $this->User_model->log($email, $password);
 
         if (!empty($check) && $check['type'] == 'User') {
-
-            $this->session->set_userdata("user_id", $check['auth_id']);
-            $this->session->set_userdata("user_email", $check['email']);
-            $this->session->set_userdata("user_mobile", $check['mobile']);
-            $this->session->set_userdata("user_type", $check['type']);
-            $check1 = $this->User_model->find_by_id($check['auth_id']);
+            $output = array('status' => 'success', 'message' => $check);
+        } else {
+            $output = array('status' => 'Error', 'message' => 'Error');
         }
-        $output = array('status' => 'success', 'message' => $check1);
+
         header('content-type: application/json');
         echo json_encode($output);
     }
@@ -64,7 +61,7 @@ class Api extends CI_Controller {
             );
 
             /////////Insert Basic Profile
-            $this->User_model->Add_detail($id,$data);
+            $this->User_model->Add_detail($id, $data);
 
             ////////Insert education Details
             for ($i = 0; $i < count($this->input->post('qualification')); $i++) {
@@ -126,44 +123,42 @@ class Api extends CI_Controller {
             } else {
                 $output = array('status' => 'error', 'message' => 'Enter Password');
             }
+        } else {
+            $output = array('status' => 'error', 'message' => 'Details Not Found');
         }
-         else {
-                $output = array('status' => 'error', 'message' => 'Details Not Found');
-            }
 
         header('content-type: application/json');
         echo json_encode($output);
     }
-    
-    
-    public function resume_add() {
-            $user_id = $_REQUEST['auth_id'];
-            $detail = $_REQUEST['detail'];
-            $config['upload_path'] = 'C:\wamp\www\jobportal\application\Resume';
-            $config['allowed_types'] = 'pdf|doc|docx';
-            $config['max_size'] = '4096';
-            $new_name = time();
-            $config['file_name'] = $new_name;
-            $this->load->library('upload', $config);
-            $this->upload->display_errors('', '');
-            $this->form_validation->set_rules('detail', 'client', 'trim|required');
-            if (!$this->upload->do_upload("resume")) {
-                echo $this->upload->display_errors();
-                die();
-                $this->data['error'] = array('error' => $this->upload->display_errors());
-                 $output = array('status' => 'error', 'message' => 'Details Not Found');
-            } else {
-                $upload_result = $this->upload->data();
 
-                print_r($upload_result['file_name']); //or print any valid
-                $this->User_model->resume($upload_result['file_name'], $user_id,$detail);
-                $output = array('status' => 'success', 'message' => 'Resume successfully added');
-            }
+    public function resume_add() {
+        $user_id = $_REQUEST['auth_id'];
+        $detail = $_REQUEST['detail'];
+        $config['upload_path'] = 'C:\wamp\www\jobportal\application\Resume';
+        $config['allowed_types'] = 'pdf|doc|docx';
+        $config['max_size'] = '4096';
+        $new_name = time();
+        $config['file_name'] = $new_name;
+        $this->load->library('upload', $config);
+        $this->upload->display_errors('', '');
+        $this->form_validation->set_rules('detail', 'client', 'trim|required');
+        if (!$this->upload->do_upload("resume")) {
+            echo $this->upload->display_errors();
+            die();
+            $this->data['error'] = array('error' => $this->upload->display_errors());
+            $output = array('status' => 'error', 'message' => 'Details Not Found');
+        } else {
+            $upload_result = $this->upload->data();
+
+            print_r($upload_result['file_name']); //or print any valid
+            $this->User_model->resume($upload_result['file_name'], $user_id, $detail);
+            $output = array('status' => 'success', 'message' => 'Resume successfully added');
+        }
 
 
 //            $data = array('title' => 'Resume Upload', 'content' => 'User/resume', 'view_data' => 'blank');
 //            $this->load->view('template1', $data);
-            redirect('User/resume', 'refresh');
+        redirect('User/resume', 'refresh');
     }
 
 }
