7984391270289d4d721aaf71b1ef8c06a41a371c
sadhnakaushik <sadhnasharma34@gmail.com>
2016-05-04 19:09:02 +0530

    add

--- application/views/job/view_applied_job.php
+++ application/views/job/view_applied_job.php
@@ -1,10 +1,29 @@
-<div class="row">
-    <div class="col-lg-12">
-        <h3 class="page-header"> Applied  Jobs List</h3>
+<div class="document-title">
+    <div class="container">
+        <h1 class="center"> Applied  Jobs List</h1>
+    </div><!-- /.container -->
+</div>
+<div class="col-sm-12">
+    <div class="tab">
+        <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" ><a href="<?php echo site_url('Job/job_list'); ?>" aria-controls="Name" >Job</a></li>
+            <li role="presentation" class="active"><a href="<?php echo site_url('Job/candidates'); ?>" aria-controls="Candidates" >Candidates</a></li>
+
+        </ul>
     </div>
 </div>
+<div class="col-sm-12">
+    <table class="table table-striped">
+        <tr>
+            <th>Name</th>
+            <th>Location</th>
+            <th>Current/Last Role</th>
+            <th>Education</th>
+            <th>Date</th>
+        </tr>
+   
 <?php foreach ($user as $u): ?>
-
+ <tr>
     <div class="col-lg-11 row panel panel-default">
         <div class="">
             <div class="col-lg-10">
@@ -27,4 +46,6 @@
 endforeach;
 //print_r($this->session->all_userdata());
 ?>
+    </table>
+</div>
    
